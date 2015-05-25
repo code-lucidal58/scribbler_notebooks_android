4e1418be774bbc3bcda259eb9d8f81ba9dcbd296
Aanisha Mishra <aanisha.mishra05@gmail.com>
2015-05-25 10:42:01 +0530

    no message

--- app/src/main/java/com/scribblernotebooks/scribblernotebooks/Activities/NavigationDrawer.java
+++ app/src/main/java/com/scribblernotebooks/scribblernotebooks/Activities/NavigationDrawer.java
@@ -108,12 +108,11 @@ public class NavigationDrawer extends AppCompatActivity implements ProfileFragme
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
         if(getSupportFragmentManager().getBackStackEntryCount()>0){
             getSupportFragmentManager().popBackStack();
         }
         else
-            finish();
+            this.finish();
     }
 
     @Override
