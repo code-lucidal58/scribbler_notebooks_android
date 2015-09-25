0532865dadbe6f07b0fe3696a058b070da81f485
Aanisha Mishra <aanisha.mishra05@gmail.com>
2015-09-25 20:46:46 +0530

    Settings Activity with toolbar

--- app/src/main/java/com/scribblernotebooks/scribblernotebooks/Activities/SettingsActivity.java
+++ app/src/main/java/com/scribblernotebooks/scribblernotebooks/Activities/SettingsActivity.java
@@ -7,25 +7,38 @@ import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.AppCompatCheckedTextView;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatRadioButton;
+import android.support.v7.widget.AppCompatSpinner;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.scribblernotebooks.scribblernotebooks.HelperClasses.Constants;
 import com.scribblernotebooks.scribblernotebooks.R;
 
 /**
+ * Setting Activity to hold users choices
  * Created by Aanisha on 02-Jun-15.
  */
 public class SettingsActivity extends PreferenceActivity {
 
-    private static final boolean ALWAYS_SIMPLE_PREFS = false;
+    //private static final boolean ALWAYS_SIMPLE_PREFS = false;
     SharedPreferences notificationPreferences;
     SharedPreferences.Editor editor;
+    static Toolbar toolbar;
+    static LinearLayout root;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        notificationPreferences = getSharedPreferences(Constants.PREF_NOTIFICATION_NAME,MODE_PRIVATE);
-        editor=notificationPreferences.edit();
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new AppSettingsFragment()).commit();
+        notificationPreferences = getSharedPreferences(Constants.PREF_NOTIFICATION_NAME, MODE_PRIVATE);
+        editor = notificationPreferences.edit();
+
     }
 
     public static class AppSettingsFragment extends PreferenceFragment {
@@ -34,6 +47,50 @@ public class SettingsActivity extends PreferenceActivity {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
         }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        root = (LinearLayout) findViewById(android.R.id.list).getParent().getParent().getParent();
+        toolbar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.pref_toolbar, root, false);
+        root.addView(toolbar, 0); // insert at top
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        addPreferencesFromResource(R.xml.preferences);
+        //getFragmentManager().beginTransaction().add(android.R.id.content, new AppSettingsFragment()).commit();
+    }
+
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        // Allow super to try and create a view first
+        final View result = super.onCreateView(name, context, attrs);
+        if (result != null) {
+            return result;
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            // If we're running pre-L, we need to 'inject' our tint aware Views in place of the
+            // standard framework versions
+            switch (name) {
+                case "EditText":
+                    return new AppCompatEditText(this, attrs);
+                case "Spinner":
+                    return new AppCompatSpinner(this, attrs);
+                case "CheckBox":
+                    return new AppCompatCheckBox(this, attrs);
+                case "RadioButton":
+                    return new AppCompatRadioButton(this, attrs);
+                case "CheckedTextView":
+                    return new AppCompatCheckedTextView(this, attrs);
+            }
+        }
+
+        return null;
     }
 
     @Override
@@ -52,14 +109,13 @@ public class SettingsActivity extends PreferenceActivity {
 
     /**
      * Determines whether the simplified settings UI should be shown. This is
-     * true if this is forced via {@link #ALWAYS_SIMPLE_PREFS}, or the device
+     * true if the device
      * doesn't have newer APIs like {@link PreferenceFragment}, or the device
      * doesn't have an extra-large screen. In these cases, a single-pane
      * "simplified" settings UI should be shown.
      */
     private static boolean isSimplePreferences(Context context) {
-        return ALWAYS_SIMPLE_PREFS
-                || Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB
                 || !isXLargeTablet(context);
     }
 }
